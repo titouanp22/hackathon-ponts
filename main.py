153bd44d48b76b07be1c2bcd56cb63e0baa4fbad
Filomène Roquefort <filomene.roquefort@gmail.com>
2023-09-08 11:13:51 +0200

    correction de certaines erreurs python

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from src.utils.ask_question_to_pdf import ask_question_to_pdf
 from src.utils.ask_question_to_pdf import gpt3_question
 from src.utils.ask_question_to_pdf import gpt3_answer
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -13,15 +14,9 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-from flask import render_template
-
-
 @app.route("/hello/")
 def hello():
     return render_template("index.html")
-
-
-from flask import request
 
 
 @app.route("/prompt", methods=["POST"])
